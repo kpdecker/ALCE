65d414c79cc2137ec7635d665724e8c8e1a64a33
kpdecker <kpdecker@gmail.com>
2013-09-05 21:19:58 -0500

    Implement calcIndent helper

--- test/util.js
+++ test/util.js
@@ -1,6 +1,22 @@
 var util = require('../lib/util');
 
 describe('util', function() {
+  describe('#calcIndent', function() {
+    it('should look at the last line only', function() {
+      console.log(util);
+      util.calcIndent('\n\n  \n\n    ').should.equal('    ');
+    });
+    it('should strip comments', function() {
+      util.calcIndent('  /* foo   bar */  ').should.equal('    ');
+      util.calcIndent('  // foo   bar   ').should.equal('  ');
+      util.calcIndent('  /* foo \n  bar */  ').should.equal('    ');
+    });
+
+    it('should count from last control character', function() {
+      util.calcIndent('    ,  ').should.equal('  ');
+    });
+  });
+
   describe('#extractRange', function() {
     it('should return range excluding wrapper', function() {
       util.extractRange('foobarfoo', [4, 7]).should.equal('bar');
